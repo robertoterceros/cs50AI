8c9fe1b5863ea5ce9a3ec3c2c372be1c8c301ff4
robertoterceros <roberto.terceros@outlook.com>
2020-04-13 22:37:07 -0400

    Started tictactoe

--- Week 0/tictactoe/tictactoe.py
+++ Week 0/tictactoe/tictactoe.py
@@ -22,27 +22,57 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # If empty then x
+    # if quant(x) = quant(y) then x
+    # else y
+    count_o = 0
+    count_x = 0
+    for i in board:
+        for j in board:
+            if board[i][j] == 'X':
+                count_x += 1
+            elif board[i][j] == 'O':
+                count_o += 1
+        if count_o == 0 and count_x == 0:
+            return 'X'
+        elif count_o == count_x:
+            return 'X'
+        else:
+            return 'O'
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    # Return all empty cases (i,j) i is the row, and j the column
+    possible_actions = set()
+    for i in board:
+        for j in board:
+            if board[i][j] == None:
+                possible_actions.add((i,j))
+    return possible_actions
+
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # Make a deep copy for new_board
+    import copy
+    new_board = copy.deepcopy(board)
+    i,j = action
+    # new_board[action] = o,x
+    new_board[i][j] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    
     raise NotImplementedError
 
 
